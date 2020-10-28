c76eb6c034cb832a45d2edf39132b39b0998926c
RobbeRDG <robbedegroeve@gmail.com>
2020-10-28 14:28:17 +0100

    Finished polyAdd exercise

---
+++ Labo2/testPolyAdd.m
@@ -0,0 +1,8 @@
+%Author: Robbe De Groeve
+
+%Initialize the two polynomes
+polyNome1 = [9 -2 -15 3 1];
+polyNome2 = [3 2];
+
+%Calculate the sum using the polyAdd function
+sum = polyAdd(polyNome1, polyNome2);
